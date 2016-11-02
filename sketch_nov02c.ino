bcc3e37be48b5a512b2fdf64f30ea85ae585acaa
cannonerd <ihmis.suski@gmail.com>
2016-11-02 23:18:11 +0100

    second iteration of soldering and rgb's to match

--- sketch_nov02c.ino
+++ sketch_nov02c.ino
@@ -11,9 +11,9 @@
     //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
     //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
     //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-    Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN0, NEO_GRB + NEO_KHZ800);
+    Adafruit_NeoPixel strip = Adafruit_NeoPixel(7, PIN0, NEO_GRB + NEO_KHZ800);
     Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(1, PIN1, NEO_GRB + NEO_KHZ800);
-    Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(1, PIN2, NEO_GRB + NEO_KHZ800);
+    Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(3, PIN2, NEO_GRB + NEO_KHZ800);
      
     void setup() {
       strip.begin();
@@ -29,17 +29,30 @@
       //(colour), wait
       colorWipe(strip.Color(0, 0, 50), 50); // blue
       colorWipe1(strip1.Color(0, 50, 0), 50); // green
-      colorWipe2(strip2.Color(50, 0, 0), 50); // red
+      colorWipe2(strip2.Color(50, 0, 0), 2000); // red
     }
      
     // Fill the dots one after the other with a color
-    void colorWipe(uint32_t c, uint8_t wait) {
-      for(uint16_t i=0; i<strip.numPixels(); i++) {
-          strip.setPixelColor(i, c);
-          strip.show();
-          delay(wait);
-      }
-     
+    void colorWipe(uint32_t cb, uint8_t wait) {
+      uint32_t r =strip.Color(50, 0, 0);
+      uint32_t g =strip.Color(0, 50, 0);
+      uint32_t b =strip.Color(0, 0, 50);
+
+   
+      strip.setPixelColor(0,r);
+      
+      strip.setPixelColor(1,g);
+      
+      strip.setPixelColor(2,b);
+      
+      strip.setPixelColor(3,r);
+      
+      strip.setPixelColor(4,g);
+      
+      strip.setPixelColor(5,b);
+      
+      strip.setPixelColor(6,r);
+      strip.show();
     }
 
 
@@ -54,9 +67,11 @@
 
       // Fill the dots one after the other with a color
     void colorWipe2(uint32_t c, uint8_t wait) {
-      for(uint16_t i=0; i<strip.numPixels(); i++) {
-          strip2.setPixelColor(i, c);
+          uint32_t r =strip.Color(50, 0, 0);
+          uint32_t g =strip.Color(0, 50, 0);
+          uint32_t b =strip.Color(0, 0, 50);
+          strip2.setPixelColor(0, r);
+          strip2.setPixelColor(1, g);
+          strip2.setPixelColor(2, b);
           strip2.show();
-          delay(wait);
-      }
     }
